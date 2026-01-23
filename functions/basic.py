16843227ceaaa15f4116578c9d2d6ad4b5391e7e
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2026-01-23 17:35:35 +0530

    Added file basic.py

---
+++ functions/basic.py
@@ -0,0 +1,8 @@
+import numpy as np
+arr=np.array([10,20,30,40,50])
+print("Values after their sum is",np.sum(arr))
+print("Values after their mean is",np.mean(arr))
+print("Values which is minimum ",np.min(arr))
+print("Values which is maximum ",np.max(arr))
+print("Values after their standard deviation is",np.std(arr))
+print("Values after their variance  is",np.var(arr))
