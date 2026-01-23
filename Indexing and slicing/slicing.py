f00e288cf57965da336a83880495ab358d9bb455
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2026-01-23 17:35:36 +0530

    Added file slicing.py

---
+++ Indexing and slicing/slicing.py
@@ -0,0 +1,6 @@
+import numpy as np
+arr=np.array([10,20,30,40,50,60])
+print("Element from 1 to 4 is",arr[1:5])
+print("Element from 0 to 3 is",arr[:4])
+print(" Every second Element is",arr[::2])
+print(" Reverse element is",arr[::-1])
