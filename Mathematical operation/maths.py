c9f5e6dd627417658c3153479b519bf850b70226
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2026-01-23 17:35:36 +0530

    Added file maths.py

---
+++ Mathematical operation/maths.py
@@ -0,0 +1,8 @@
+import numpy as np
+arr=np.array([10,20,30,40,50])
+print("Values after addition is",arr+5)
+print("Values after subtraction is",arr-5)
+print("Values after multiplication is",arr*5)
+print("Values after twice the power is",arr**2)
+print("Values after remainder is",arr%3)
+print("Values after division is",arr/2)
