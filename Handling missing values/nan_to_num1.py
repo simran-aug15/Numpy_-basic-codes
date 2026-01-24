f6104701d59374b10c83b70b36c0212ea0c868c6
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2026-01-24 14:57:16 +0530

    Auto commit

--- Handling missing values/nan_to_num1.py
+++ Handling missing values/nan_to_num1.py
@@ -2,5 +2,5 @@
 import numpy as np
 arr=np.array([1,2,np.inf,4,np.inf,6])
 print(np.isinf(arr))
-cleaned_arr=np.nan_to_num(arr,posinf=100,neging=-100)
+cleaned_arr=np.nan_to_num(arr,posinf=100,neginf=-100)
 print(cleaned_arr)
