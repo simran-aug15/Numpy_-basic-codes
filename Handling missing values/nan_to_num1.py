920f6bb71043ec2130bba12c46369c88397c5f24
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2026-01-24 14:56:47 +0530

    Auto commit

---
+++ Handling missing values/nan_to_num1.py
@@ -0,0 +1,6 @@
+"""nan_to_num=to replace the infinite value"""
+import numpy as np
+arr=np.array([1,2,np.inf,4,np.inf,6])
+print(np.isinf(arr))
+cleaned_arr=np.nan_to_num(arr,posinf=100,neging=-100)
+print(cleaned_arr)
