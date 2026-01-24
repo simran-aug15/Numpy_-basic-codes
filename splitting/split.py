1f24b4c1fb1379259f504c7b4e84325ef1868846
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2026-01-24 14:30:37 +0530

    Auto commit

---
+++ splitting/split.py
@@ -0,0 +1,8 @@
+"""np.split()=divide in equal parts
+   np.hsplit()=Horizontal split
+   np.vsplit()=vertical split
+"""
+
+import numpy as np
+arr=np.array([10,20,30,40,50,60])
+print(np.split(arr,2))
