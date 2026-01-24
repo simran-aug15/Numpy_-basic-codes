33667a2ae5fa537f5662f02cdd6a958873da55d7
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2026-01-24 14:26:58 +0530

    Auto commit

--- stacking/stacking.py
+++ stacking/stacking.py
@@ -5,5 +5,5 @@
 import numpy as np
 arr1=np.array([1,2,3])
 arr2=np.array([4,5,6])
-print("vertical element is",np.vstack(arr1,arr2))
-print("Horizontal element is",np.hstack(arr1,arr2))
+print("vertical element is",np.vstack((arr1,arr2)))
+print("Horizontal element is",np.hstack((arr1,arr2)))
